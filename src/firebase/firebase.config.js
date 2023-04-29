e1876085beec79d48019fb2094b534874f684146
riyad3399 <programminghero.riyad@gmail.com>
2023-04-29 23:56:48 +0600

    modify

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,15 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 
-console.log('environment variable', import.meta.env.VITE_authDomain);
+console.log('environment variable', import.meta.env.VITE_APPID);
 
 const firebaseConfig = {
-apiKey:import.meta.env.VITE_APIKEY , 
-authDomain:import.meta.env.VITE_AUTHDOMAIN , 
-projectId:import.meta.env.VITE_PROJECTID , 
-storageBucket:import.meta.env.VITE_STORAGEBUCKET , 
-messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
-appId:import.meta.env.VITE_APPID , 
+apiKey: import.meta.env.VITE_APIKEY, 
+authDomain: import.meta.env.VITE_AUTHDOMAIN, 
+projectId: import.meta.env.VITE_PROJECTID, 
+storageBucket: import.meta.env.VITE_STORAGEBUCKET, 
+messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID ,
+appId: import.meta.env.VITE_APPID, 
 };
 
 // Initialize Firebase
